e15c38302f9fb961692c038dbb24d3b5973aa6ab
Joshua Neipp <jneipp@cisco.com>
2019-03-12 21:22:30 -0400

    Revert uniqueness validation on phone number for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   store_accessor :settings, :zip_code, :notify_of_frost_warning
   has_secure_password
   validates :username, presence: true, uniqueness: true
-  validates :sms_number, uniqueness: true
+  # validates :sms_number, uniqueness: true
   before_create :set_next_forecast_check_time
   before_create :set_sms_verification_code
 
