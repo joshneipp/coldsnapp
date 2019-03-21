80b03391199d6f0c68cb4e734b70e1b984b08f9b
Joshua Neipp <jneipp@cisco.com>
2019-03-21 11:24:15 -0400

    Update forecast worker

--- app/workers/user_forecast_worker.rb
+++ app/workers/user_forecast_worker.rb
@@ -12,7 +12,9 @@ class UserForecastWorker
     users_for_forecast.each do |user|
       if user.settings['notify_of_frost']
         zip_code = user.settings['zip_code']
+        Rails.logger.info "#{Time.zone.now} -- #{self.class.to_s} -- Fetching forecast for #{user.id}"
         forecast = ForecastCacheService.new(zip_code).run
+        Rails.logger.info "Forecast failed...." unless forecast
         if forecast && forecast.any? { |day| day < 32 }
           user_message_params = {
             from: ENV['TWILIO_TEST_SMS_FROM'],
@@ -21,11 +23,12 @@ class UserForecastWorker
             body: 'There is a forecast of frost in your area in the next 7 days. This is a test from user_forecast_worker.rb'
           }
           TwilioService.new.send_message(user_message_params)
+        else
+          Rails.logger.info "#{self.class.to_s} No frost forecast for user #{user.id}"
         end
-        Rails.logger.debug "#{Time.zone.now} -- #{self.class.to_s} -- Fetching forecast for #{user.id}"
+        Rails.logger.debug "#{Time.zone.now} -- #{self.class.to_s} -- Updating user with ids #{users_for_forecast.pluck(:id)}"
+        users_for_forecast.update_all(next_forecast_check_time: (Time.now + 1.day))
       end
     end
-    users_for_forecast.update_all(next_forecast_check_time: (Time.now + 1.day))
-    Rails.logger.debug "#{Time.zone.now} -- #{self.class.to_s} -- Updating user with ids #{users_for_forecast.pluck(:id)}"
   end
 end
