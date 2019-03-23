b2bd73059092a5f252a533ed97e77c3a9829395e
Joshua Neipp <jneipp@cisco.com>
2019-03-22 20:03:26 -0400

    Fix sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ require 'sidekiq-scheduler/web'
 Rails.application.routes.draw do
   root to: 'user_registrations#new'
   match 'signup', to: 'user_registrations#new', via: :get
-  # get 'signup', to: 'user_registrations#new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount Sidekiq::Web => '/sidekiq'
-
-  # resources :user_registrations, only: [:new, :create, :show]
 
   resources :user_registrations, only: [:new, :create]
   resources :user_verifications, only: [:new, :create]
   get 'signup', to: 'user_registrations#new'
   get 'verify', to: 'user_verifications#new'
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV['SIDEKIQ_USERNAME'] && password == ENV['SIDEKIQ_PASSWORD']
+  end if Rails.env.production?
+
+  mount Sidekiq::Web => '/sidekiq'
 end
