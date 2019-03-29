0d22bc72a63c8d44b9af5ad5ff0da5a05aa0267a
Joshua Neipp <jneipp@cisco.com>
2019-03-28 21:47:00 -0400

    Config sendgrid

--- app/services/sendgrid_service.rb
+++ app/services/sendgrid_service.rb
@@ -4,7 +4,7 @@ include SendGrid
 class SendgridService
 
   def initialize
-    @client = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://cors-anywhere.herokuapp.com/https://api.sendgrid.com').client
+    @client = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com').client
   end
 
   def run(from, subject, to, content)
