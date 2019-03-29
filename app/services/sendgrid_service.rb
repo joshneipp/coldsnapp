b9bfdcf8f5a24dfc2312e556815f908d0fa79f53
Joshua Neipp <jneipp@cisco.com>
2019-03-28 21:36:50 -0400

    Update sendgrid

--- app/services/sendgrid_service.rb
+++ app/services/sendgrid_service.rb
@@ -4,7 +4,7 @@ include SendGrid
 class SendgridService
 
   def initialize
-    @client = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com').client
+    @client = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://cors-anywhere.herokuapp.com/https://api.sendgrid.com').client
   end
 
   def run(from, subject, to, content)
@@ -13,7 +13,7 @@ class SendgridService
     content = Content.new(type: 'text/plain', value: content)
     mail = Mail.new(from, subject, to, content)
 
-    Rails.logger.info "#{Time.zone.now} -- #{self.class.to_s} -- mail #{mail}...."
+    Rails.logger.info "#{Time.zone.now} -- #{self.class.to_s} -- mail #{mail.to_json}...."
 
     @client.mail._('send').post(request_body: mail.to_json)
   end
